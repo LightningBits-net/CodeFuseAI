493358f57f4feb68e30c1d81186be49b84c04c57
Lightningbits <rafaelmorel@lightningbits.com>
2023-08-02 19:10:23 -0400

    Fix Bug, Update packages, Initial db test

---
+++ CodeFuseAI_Shared/Migrations/20230802124531_CodeFuseAI_Intial1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFuseAI_Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class CodeFuseAI_Intial1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
